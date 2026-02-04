2f409c68b083ca00a90910d4b7f73d5c00700741
Sonia <kakkarsonia143@gmail.com>
2026-02-04 12:04:04 -0400

    februray4

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <AppRoutes /> { /* Children prop component */}
-    </BrowserRouter>
-  </StrictMode>,
+<StrictMode>
+<QueryClientProvider client={queryClient}>
+<BrowserRouter>
+<AppRoutes /> { /* Children prop component */}
+</BrowserRouter>
+<ReactQueryDevtools initialIsOpen={false} />
+</QueryClientProvider>
+</StrictMode>,
+
 )
+ 
--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,10 +1,43 @@
+import { useQuery } from "@tanstack/react-query";
+import CustomerTable from "../components/customers/CustomerTable";
+import { FaSpinner } from "react-icons/fa";
+
+const fetchCustomers = async () => {
+  const response = await fetch("http://localhost:3000/customers");
+  const data = await response.json();
+  return data;
+};
+
 const Customers = () => {
+  const {
+    data: customers = [],
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["customerCache"],
+    queryFn: fetchCustomers,
+  });
+
   return (
     <div>
       <h1 className="text-2xl font-bold">Customers</h1>
       <p>Welcome to the admin Customers.</p>
+      {error ? (
+        <p className="text-red-500">
+          Error fetching customers: {error.message}
+        </p>
+      ) : isLoading ? (
+        <div className="flex items-center justify-center">
+          <FaSpinner className="animate-spin mr-2" />
+          <p>Loading customers...</p>
+        </div>
+      ) : (
+        <CustomerTable customers={customers} />
+      )}
     </div>
   );
 };
 
 export default Customers;
+
+ 
